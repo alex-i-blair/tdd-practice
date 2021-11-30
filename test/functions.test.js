4fc9f1ec0449bdc461f3cd9f911c1420babf0847
Alex Blair <bclan11@gmail.com>
2021-11-30 14:31:31 -0800

    semicolon

--- test/functions.test.js
+++ test/functions.test.js
@@ -8,7 +8,7 @@ const { test, skip } = QUnit;
 test('addExclamationPoints should return a string with 3 exclamation points added to the end', (expect) => {
     const expected1 = 'puppy!!!';
     const actual1 = addExclamationPoints('puppy');
-    const expected2 = 'muppy!!!'
+    const expected2 = 'muppy!!!';
     const actual2 = addExclamationPoints('muppy');
 
     expect.equal(actual1, expected1, 'should return "puppy!!!"');
